662be0f51b079387a61be9e6755cacd37a8d1ad3
YYZ <yuyuezhang0550@163.com>
2021-02-24 20:24:44 +0800

    2.24

--- src/Solution.java
+++ src/Solution.java
@@ -4,26 +4,26 @@ public class Solution {
 
     //给定一个非空且只包含非负数的整数数组 nums，数组的度的定义是指数组里任一元素出现频数的最大值。
     //你的任务是在 nums 中找到与 nums 拥有相同大小的度的最短连续子数组，返回其长度。
-    public int findShortestSubArry(int[] nums){
-        Map<Integer,int[]> map = new HashMap<Integer, int[]>();
+    public int findShortestSubArry(int[] nums) {
+        Map<Integer, int[]> map = new HashMap<Integer, int[]>();
         int n = nums.length;
-        for(int i =0; i < n; i++){
-            if(map.containsKey(nums[i])) {
+        for (int i = 0; i < n; i++) {
+            if (map.containsKey(nums[i])) {
                 map.get(nums[i])[0]++;
                 map.get(nums[i])[2] = i;
             } else {
-                map.put(nums[i], new int[]{1,i,i});
+                map.put(nums[i], new int[]{1, i, i});
             }
         }
         int maxNum = 0, minLen = 0;
-        for(Map.Entry<Integer,int[]> entry : map.entrySet()){
-            int[] arr = entry.getValue() ;
-            if(maxNum < arr[0]){
+        for (Map.Entry<Integer, int[]> entry : map.entrySet()) {
+            int[] arr = entry.getValue();
+            if (maxNum < arr[0]) {
                 maxNum = arr[0];
-                minLen = arr[2] - arr[1] +1;
-            } else if (maxNum == arr[0]){
-                if (minLen > arr[2] - arr[1] +1){
-                    minLen = arr[2] - arr[1] +1;
+                minLen = arr[2] - arr[1] + 1;
+            } else if (maxNum == arr[0]) {
+                if (minLen > arr[2] - arr[1] + 1) {
+                    minLen = arr[2] - arr[1] + 1;
                 }
             }
 
@@ -33,14 +33,14 @@ public class Solution {
 
 
     //1. 两数之和
-    public int[] twoSum(int[]nums, int target){
+    public int[] twoSum(int[] nums, int target) {
         int[] ans = new int[2];
-        for(int i= 0; i<nums.length ; i++){
-            for(int j=i+1; j<nums.length; j++){
-                if(target == nums[i]+ nums[j]){
+        for (int i = 0; i < nums.length; i++) {
+            for (int j = i + 1; j < nums.length; j++) {
+                if (target == nums[i] + nums[j]) {
 
-                    ans[0]=i;
-                    ans[1]=j;
+                    ans[0] = i;
+                    ans[1] = j;
                     return ans;
                 }
             }
@@ -49,13 +49,13 @@ public class Solution {
     }
 
     //1. 两数之和hash
-    public int[] twoSumHash(int[]nums, int target){
-        Map<Integer,Integer> hashtable = new HashMap<Integer, Integer>();
-        for (int i =0; i<nums.length; i++){
-            if (hashtable.containsKey(target - nums[i])){
-                return new int[]{hashtable.get(target-nums[i]),i};
+    public int[] twoSumHash(int[] nums, int target) {
+        Map<Integer, Integer> hashtable = new HashMap<Integer, Integer>();
+        for (int i = 0; i < nums.length; i++) {
+            if (hashtable.containsKey(target - nums[i])) {
+                return new int[]{hashtable.get(target - nums[i]), i};
             }
-            hashtable.put(nums[i],i);
+            hashtable.put(nums[i], i);
         }
         return new int[0];
     }
@@ -64,30 +64,142 @@ public class Solution {
     public List<List<Integer>> permute(int[] nums) {
         List<List<Integer>> res = new ArrayList<List<Integer>>();
         List<Integer> output = new ArrayList<Integer>();
-        for(int num : nums){
+        for (int num : nums) {
             output.add(num);
         }
 
         int n = nums.length;
-        backtrack(n,output,res,0);
+        backtrack(n, output, res, 0);
         return res;
     }
 
-    public void backtrack(int n, List<Integer> output, List<List<Integer>> res, int first){
+    public void backtrack(int n, List<Integer> output, List<List<Integer>> res, int first) {
         // 所有数都填完了
-        if(first == n){
+        if (first == n) {
             res.add(new ArrayList<Integer>(output));
         }
-        for(int i = first; i < n ; i++){
+        for (int i = first; i < n; i++) {
             // 动态维护数组
-            Collections.swap(output,first,i);
+            Collections.swap(output, first, i);
             // 继续递归填下一个数
-            backtrack(n,output,res,first+1);
+            backtrack(n, output, res, first + 1);
             // 撤销操作
-            Collections.swap(output,first,i);
+            Collections.swap(output, first, i);
         }
     }
     //时间复杂度：O(n \times n!)O(n×n!)，其中 nn 为序列的长度。
     //空间复杂度：O(n)O(n)，其中 nn 为序列的长度。
 
+    //832 翻转图像
+    public int[][] flipAndInvertImage(int[][] A) {
+        int n = A.length;
+        for (int i = 0; i < n; i++) {
+            for (int j = 0; j < n / 2; j++) {
+                if (A[i][j] == A[i][n - j - 1]) {
+                    A[i][j] ^= 1;
+                    A[i][n - j - 1] ^= 1;
+                }
+            }
+            if (n % 2 != 0)
+                A[i][n / 2] ^= 1;
+        }
+        return A;
+    }
+
+    //2.两数相加
+    private class ListNode {
+        int val;
+        ListNode next;
+
+        ListNode() {
+        }
+
+        ListNode(int val) {
+            this.val = val;
+        }
+
+        ListNode(int val, ListNode next) {
+            this.val = val;
+            this.next = next;
+        }
+    }
+
+    public ListNode addTwoNumbers(ListNode l1, ListNode l2) {
+        ListNode head = null, tail = null;
+        int carry = 0;
+        while (l1 != null || l2 != null) {
+            int n1 = l1 != null ? l1.val : 0;
+            int n2 = l2 != null ? l2.val : 0;
+            int sum = n1 + n2 + carry;
+            if (head == null) {
+                head = tail = new ListNode(sum % 10);
+            } else {
+                tail.next = new ListNode(sum % 10);
+                tail = tail.next;
+            }
+            carry = sum / 10;
+            if (l1 != null) l1 = l1.next;
+            if (l2 != null) l2 = l2.next;
+        }
+        //链表最后需要进位
+        if (carry > 0)
+            tail.next = new ListNode(carry);
+        return head;
+    }
+
+    //4 中位数
+    public double findMedianSortedArrays(int[] nums1, int[] nums2) {
+        int length1 = nums1.length, length2 = nums2.length;
+        int totalLength = length1 + length2;
+        if (totalLength % 2 == 1) {
+            int midIndex = totalLength / 2;
+            double median = getKthElement(nums1, nums2, midIndex + 1);
+            return median;
+        } else {
+            int midIndex1 = totalLength / 2 - 1, midIndex2 = totalLength / 2;
+            double median = (getKthElement(nums1, nums2, midIndex1 + 1) + getKthElement(nums1, nums2, midIndex2 + 1)) / 2.0;
+            return median;
+        }
+    }
+
+    public int getKthElement(int[] nums1, int[] nums2, int k) {
+        /* 主要思路：要找到第 k (k>1) 小的元素，那么就取 pivot1 = nums1[k/2-1] 和 pivot2 = nums2[k/2-1] 进行比较
+         * 这里的 "/" 表示整除
+         * nums1 中小于等于 pivot1 的元素有 nums1[0 .. k/2-2] 共计 k/2-1 个
+         * nums2 中小于等于 pivot2 的元素有 nums2[0 .. k/2-2] 共计 k/2-1 个
+         * 取 pivot = min(pivot1, pivot2)，两个数组中小于等于 pivot 的元素共计不会超过 (k/2-1) + (k/2-1) <= k-2 个
+         * 这样 pivot 本身最大也只能是第 k-1 小的元素
+         * 如果 pivot = pivot1，那么 nums1[0 .. k/2-1] 都不可能是第 k 小的元素。把这些元素全部 "删除"，剩下的作为新的 nums1 数组
+         * 如果 pivot = pivot2，那么 nums2[0 .. k/2-1] 都不可能是第 k 小的元素。把这些元素全部 "删除"，剩下的作为新的 nums2 数组
+         * 由于我们 "删除" 了一些元素（这些元素都比第 k 小的元素要小），因此需要修改 k 的值，减去删除的数的个数
+         */
+
+        int length1 = nums1.length, length2 = nums2.length;
+        int index1 = 0, index2 = 0;
+        int kthElement = 0;
+
+        while (true) {
+            //边界情况
+            if (index1 == length1)
+                return nums2[index2 + k - 1];
+            if (index2 == length2)
+                return nums1[index1 + k - 1];
+            if (k == 1)
+                return Math.min(nums1[index1], nums2[index2]);
+
+            //正常情况
+            int half = k / 2;
+            int newIndex1 = Math.min(index1 + half, length1) - 1;
+            int newIndex2 = Math.min(index2 + half, length2) - 1;
+            int pivot1 = nums1[newIndex1], pivot2 = nums2[newIndex2];
+            if (pivot1 <= pivot2) {
+                k -= (newIndex1 - index1 + 1);
+                index1 = newIndex1 + 1;
+            } else {
+                k -= (newIndex2 - index2 + 1);
+                index2 = newIndex2 + 1;
+            }
+
+        }
+    }
 }
--- src/test.java
+++ src/test.java
@@ -1,27 +1,33 @@
 import java.util.*;
 
 public class test {
-    public List<List<Integer>> shownum(int[] nums) {
-        List<List<Integer>> res = new ArrayList<List<Integer>>();
-        List<Integer> output = new ArrayList<Integer>();
-        for (int num : nums) {
-            output.add(num);
-        }
 
-        int n = nums.length;
-        backtrack(n, output, res, 0);
-        return res;
+    private class ListNode{
+        int val;
+        ListNode next;
+        ListNode(int val){this.val = val;}
+        ListNode(int val,ListNode next){this.val = val; this.next = next;}
     }
 
-    public void backtrack(int n, List<Integer> output, List<List<Integer>> res, int first) {
-        if (n == first) {
-            res.add(new ArrayList<Integer>(output));
+    public ListNode add (ListNode l1,ListNode l2){
+        ListNode head = null , tail = null;
+        int n1,n2,sum,carry=0;
+        while(l1 != null || l2 !=null){
+            n1 = l1 !=null ? l1.val : 0;
+            n2 = l2 !=null ? l1.val : 0;
+            sum =n1+n2+carry;
+            if(head == null)
+                head = tail = new ListNode(sum%10);
+            else{
+                tail = new ListNode(sum%10);
+                tail = tail.next;
+            }
+            carry = sum/10;
+            if (l1 !=null) l1 = l1.next;
+            if (l2 !=null) l2 = l2.next;
         }
-        for (int i = 0; i < n; i++) {
-            Collections.swap(output,n,first);
-            backtrack(n,output,res,first+1);
-            Collections.swap(output,n,first);
-        }
+        if(carry>0) tail.next = new ListNode(carry);
+        return head;
     }
 
 }
